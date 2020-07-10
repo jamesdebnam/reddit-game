17a371442923d3928d0e69d76583c72d077f43f9
jamesdebnam <jamesdebnam@gmail.com>
2020-07-10 15:32:01 +0100

    implemented typescript

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="game">
+      <h1 className="game__header">The Reddit Game</h1>
+    </div>
+  );
+}
