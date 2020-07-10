cf2e1e309bff4ed95065d06f3de1e1f552a1eaf2
jamesdebnam <jamesdebnam@gmail.com>
2020-07-10 15:41:06 +0100

    Added difficulty radio form

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./App.css";
+import DifficultyForm from "../DifficultyForm/DifficultyForm";
 
 export default function App() {
   return (
     <div className="game">
       <h1 className="game__header">The Reddit Game</h1>
+      <DifficultyForm />
     </div>
   );
 }
