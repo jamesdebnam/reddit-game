39a95806d3eacbf798da4a6c089d3343377e9e6a
jamesdebnam <jamesdebnam@gmail.com>
2020-07-10 15:49:14 +0100

    added basic form styling

--- src/DifficultyForm/DifficultyForm.tsx
+++ src/DifficultyForm/DifficultyForm.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 
+import "./DifficultyForm.css";
+
 export default function DifficultyForm() {
   return (
     <div className="difficulty">
       <h2 className="difficulty__header">Choose a difficulty</h2>
       <form className="difficulty__form">
-        <div className="radio__super-easy">
-          <input type="radio" id="super-easy" value="super-easy" />
+        <div className="radio radio__super-easy">
+          <input
+            name="difficulty"
+            type="radio"
+            id="super-easy"
+            value="super-easy"
+          />
           <label htmlFor="super-easy">Super Easy</label>
         </div>
-        <div className="radio__easy">
-          <input type="radio" id="easy" value="easy" />
+        <div className="radio radio__easy">
+          <input name="difficulty" type="radio" id="easy" value="easy" />
           <label htmlFor="easy">Easy</label>
         </div>
-        <div className="radio__medium">
-          <input type="radio" id="medium" value="medium" />
+        <div className="radio radio__medium">
+          <input name="difficulty" type="radio" id="medium" value="medium" />
           <label htmlFor="medium">Medium</label>
         </div>
-        <div className="radio__hard">
-          <input type="radio" id="hard" value="hard" />
+        <div className="radio radio__hard">
+          <input name="difficulty" type="radio" id="hard" value="hard" />
           <label htmlFor="hard">Hard</label>
         </div>
       </form>
