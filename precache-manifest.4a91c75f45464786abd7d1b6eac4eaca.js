d686e3a0d07f8c14d1da20eb04a152a515cdf6ea
jamesdebnam <jamesdebnam@gmail.com>
2020-07-15 07:56:29 +0100

    Updates

---
+++ precache-manifest.4a91c75f45464786abd7d1b6eac4eaca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3d0854d65dc7ec17ddc5bab9626ea3",
+    "url": "/reddit-game/index.html"
+  },
+  {
+    "revision": "97352f0fb1bf24d91967",
+    "url": "/reddit-game/static/css/main.5d3e85b0.chunk.css"
+  },
+  {
+    "revision": "9735db2ba3cb2122f9ca",
+    "url": "/reddit-game/static/js/2.e666dd2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-game/static/js/2.e666dd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97352f0fb1bf24d91967",
+    "url": "/reddit-game/static/js/main.10a43dfc.chunk.js"
+  },
+  {
+    "revision": "06634888d8ac404b9caf",
+    "url": "/reddit-game/static/js/runtime-main.ac30a484.js"
+  }
+]);
